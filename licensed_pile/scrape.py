179f890ea91282d9a0767be09913d96b1db26a38
Brian Lester <blester125@gmail.com>
2024-01-21 23:41:13 -0500

    Use a better user-agent string

---
+++ licensed_pile/scrape.py
@@ -0,0 +1,8 @@
+"""Shared Utilities related to scraping."""
+
+
+# A user agent that says we are compatible with most websites (most browsers
+# start with Mozilla/5.0) and also tells that we are a bot and includes a link
+# for context on why we are scraping. We hope this fosters good will with site
+# owners.
+USER_AGENT = "Mozilla/5.0 (compatible; Licensed-Pile-bot/0.1; +http://www.github.com/r-three/licensed-pile)"
